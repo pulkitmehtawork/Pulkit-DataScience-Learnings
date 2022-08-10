5c24e53193d14ebc9d6f8ba4a5a2389c79e2c47e
Pulkit Mehta <pulkit_mehta_work@yahoo.com>
2022-08-10 15:37:29 +0530

    Update twosum.py

--- DSA/Arrays/twosum.py
+++ DSA/Arrays/twosum.py
@@ -7,7 +7,7 @@ class Solution:
             for j in range(len(nums)):
                 if i != j and nums[i]+nums[j] ==  target:
                     return [i,j]
-# 1. 0(n) solution
+# 2. 0(n) solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_map = {}
